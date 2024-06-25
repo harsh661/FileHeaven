306a7ec35a43776e688860e6a7fae4fa6cdeb8e7
harsh661 <plasmaharsh@gmail.com>
2024-06-25 20:23:42 +0530

    feat: show file icon based on type

--- app/(user)/_components/fileCard.tsx
+++ app/(user)/_components/fileCard.tsx
@@ -6,7 +6,10 @@ import {
 } from "@/components/ui/popover";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { RiStarLine, RiFileCopyLine } from "react-icons/ri";
+import { FaRegImage, FaRegFilePdf } from "react-icons/fa6";
 import DeleteFileDialog from './DeleteFileDialog';
+import { LuText } from "react-icons/lu";
+import { TbFileTypeCsv, TbFileZip } from "react-icons/tb";
 import { Doc } from '@/convex/_generated/dataModel';
 
 const FileCard = ({ file }: { file: Doc<"files"> }) => {
@@ -32,7 +35,15 @@ const FileCard = ({ file }: { file: Doc<"files"> }) => {
                 </Popover>
             </div>
             <div className='flex flex-col gap-2 py-1'>
-                <p className='font-medium line-clamp-2'>{file.name}</p>
+                <div className='font-medium line-clamp-2 flex items-center gap-1'>
+                    {file.type === "image" ? < FaRegImage />
+                        : file.type === "txt" ? <LuText />
+                            : file.type === "pdf" ? <FaRegFilePdf />
+                                : file.type === "csv" ? <TbFileTypeCsv />
+                                    : <TbFileZip />
+                    }
+                    <p>{file.name}</p>
+                </div>
             </div>
         </div>
     )
